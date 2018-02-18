dbaf92a6427bb91bb1a3ee8958113db4d8555203
ali <elixircist@gmail.com>
2018-02-18 10:49:21 -0500

    add logout view

--- Forum/urls.py
+++ Forum/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 	path('testRequest', views.test_request, name='test_request'),
     path('registerUser', views.register_user, name='register_user'),
     path('loginUser', views.login_user, name='login_user'),
+    path('logoutUser', views.logout_user, name='logout_user'),
 ]
