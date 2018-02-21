9aae0efcce89b28e26b29ef95fe5060fde13f441
ali <elixircist@gmail.com>
2018-02-21 15:35:44 -0500

    add forum post url

--- Forum/urls.py
+++ Forum/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('registerUser', views.register_user, name='register_user'),
     path('loginUser', views.login_user, name='login_user'),
     path('logoutUser', views.logout_user, name='logout_user'),
+    path('addForumPost', views.add_forum_post, name='add_forum_post'),
 ]
