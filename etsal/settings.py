d05047a7068f695705f4a00f3b61deab89405ad5
ali <elixircist@gmail.com>
2018-03-02 12:52:06 -0500

    allowed hosts header

--- etsal/settings.py
+++ etsal/settings.py
@@ -9,6 +9,7 @@ import django_heroku
 DEBUG = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#v8en*1-hd0vq^!%kap7pa++0$_ma_jnjlyrkh9gb!@^a^4fr='
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
     'infinite-reef-90129.herokuapp.com',
 )
