9f1997543887c72baba004c9194018445f7015da
ali <elixircist@gmail.com>
2018-03-23 16:32:56 -0400

    cors test

--- etsal/settings.py
+++ etsal/settings.py
@@ -9,9 +9,8 @@ import django_heroku
 DEBUG = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#v8en*1-hd0vq^!%kap7pa++0$_ma_jnjlyrkh9gb!@^a^4fr='
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_REGEX_WHITELIST = (r'*', )
 
 # Application definition
 INSTALLED_APPS = [
