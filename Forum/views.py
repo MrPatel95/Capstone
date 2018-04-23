4a425234dc9612fde057778435e267799e97419f
ali <elixircist@gmail.com>
2018-04-23 08:42:17 -0400

    testing reply count

--- Forum/views.py
+++ Forum/views.py
@@ -255,7 +255,7 @@ def get_n_recent_forum_posts(request):
 			posts = ForumPost.objects.filter().values(
 				'post_id', 'user__username', 'post_title', 'post_image', 
 				'post_datetime', 'connect_count', 'post_body', 'forumconnector__user'
-				).order_by('-post_datetime').annotate(reply_count=Count('replypost__post_id_id')).annotate(connected=Case(When(forumconnector__user=request.user, then=1), output_field=CharField()))[:n]
+				).order_by('-post_datetime').annotate(reply_count=Count('replypost__post_id')).annotate(connected=Case(When(forumconnector__user=request.user, then=1), output_field=CharField())).distinct()[:n]
 			return HttpResponse(json.dumps(list(posts), default=_date_handler))
 		except Exception as e:
 			return HttpResponse('{"response":"exception","error":"%s"}' % traceback.format_exc())
